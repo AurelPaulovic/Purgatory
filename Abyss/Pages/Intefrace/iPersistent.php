cc38601236f0cd19779c73c781fcf0d1baadee1a
AurelPaulovic <aurel.paulovic@gmail.com>
2012-07-09 19:31:46 +0200

    Change-Id: Icf4edc8437a5f37474f8070356ca4a385ba7537b

---
+++ Abyss/Pages/Intefrace/iPersistent.php
@@ -0,0 +1,29 @@
+<?php
+namespace Abyss\Pages;
+
+/**
+ * Requires the implementing object to be able to be persisted (serialize, unserialize, etc.)
+ *
+ * Default implementation: tAppPersistent, tUserPersistent
+ *
+ * @author APA
+ * @since 0.1
+ * @version 0.1
+ */
+interface iPersistent {
+	/**
+	 * Saves the state of the object to persistent store
+	 *
+	 * @return boolean
+	 */
+	public function save();
+
+	/**
+	 * Creates new instance of the object using the saved state or returns null
+	 *
+	 * @return Object|null
+	 */
+	public function load();
+}
+
+?>
---
+++ Abyss/Pages/Trait/tAppPersistent.php
@@ -0,0 +1,6 @@
+<?php
+namespace Abyss\Pages;
+
+trait tAppPersistent {
+
+}
